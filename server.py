1e9bd294ae19f18fa781d90b7cfb5cf3906e40fc
Thomas Squires <thomassquires951@gmail.com>
2025-09-19 12:16:22 -0400

    Change formatting in server.py to get a 10/10 on static code analysis with pylint

--- server.py
+++ server.py
@@ -24,11 +24,11 @@ def detect_emotion():
     if emotion_data['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
 
-    # Return formatted data to user 
+    # Return formatted data to user
     return f"For the given statement, the system response is 'anger': {emotion_data['anger']}, \
-     'disgust': {emotion_data['disgust']}, 'fear': {emotion_data['fear']}, 'joy': {emotion_data['joy']}, \
-     and 'sadness': {emotion_data['sadness']}. The dominant emotion is {emotion_data['dominant_emotion']}."
-
+    'disgust': {emotion_data['disgust']}, 'fear': {emotion_data['fear']}, \
+    'joy': {emotion_data['joy']}, and 'sadness': {emotion_data['sadness']}. \
+    The dominant emotion is {emotion_data['dominant_emotion']}."
 
 @app.route("/")
 def render_index_page():
@@ -38,5 +38,5 @@ def render_index_page():
     # Render index.html
     return render_template("index.html")
 
-if __name__ == "__main__":  
-    app.run(port = 5000) 
+if __name__ == "__main__":
+    app.run(port = 5000)
